1fbab28e978b8fdc86e9eb5b59b06f2d23ca786a
samluddite <56632175+SamsRepos@users.noreply.github.com>
2024-03-14 19:49:51 +0000

    initial version

---
+++ battery_alerter.py
@@ -0,0 +1,40 @@
+import psutil
+import winsound
+from time import sleep
+import os
+import win32api
+
+clear = lambda: os.system('cls')
+
+def seconds_to_hours(secs):
+    mm, ss = divmod(secs, 60)
+    hh, mm = divmod(mm, 60)
+    return "%d:%02d:%02d" % (hh, mm, ss)
+
+PERCENTAGE_THRESHOLD = 95
+SECONDS_BETWEEN_CHECKS = 8
+
+DIALOG_TITLE = "LOW BATTERY!"
+
+if __name__ == '__main__':
+  while True:
+    battery  = psutil.sensors_battery()
+    percent  = battery.percent
+    secs_left = battery.secsleft
+    plugged_in = battery.power_plugged
+
+    messages = [f"Charge = {percent}%", 
+                f"Estimated time left = {seconds_to_hours(secs_left)}",
+                "Plugged in" if plugged_in else "Not plugged in"
+               ]
+
+    clear()
+    for message in messages:
+      print(message)
+
+    if (percent < PERCENTAGE_THRESHOLD) and not plugged_in:
+      winsound.PlaySound('./sfx/alert.wav', winsound.SND_FILENAME)
+      dialog_message = "\n".join(messages)
+      win32api.MessageBox(0, dialog_message, DIALOG_TITLE, 0x00001000)
+    
+    sleep(SECONDS_BETWEEN_CHECKS)
