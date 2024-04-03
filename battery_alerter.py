e3c77a556f5e39b52ea0c0a7a14ae4b451bd8195
samluddite <56632175+SamsRepos@users.noreply.github.com>
2024-04-03 18:59:59 +0000

    Datetime added to console output

--- battery_alerter.py
+++ battery_alerter.py
@@ -3,6 +3,7 @@ import winsound
 from time import sleep
 import os
 import win32api
+from datetime import datetime
 
 clear = lambda: os.system('cls')
 
@@ -10,6 +11,10 @@ def seconds_to_hours(secs):
     mm, ss = divmod(secs, 60)
     hh, mm = divmod(mm, 60)
     return "%d:%02d:%02d" % (hh, mm, ss)
+
+def datetime_formatted(dt):
+  return dt.strftime('%H:%M:%S %d/%m/%Y')
+#  return dt.strftime('%d/%m/%Y, %H:%M:%S')
 
 PERCENTAGE_THRESHOLD = 20
 SECONDS_BETWEEN_CHECKS = 8
@@ -31,6 +36,7 @@ if __name__ == '__main__':
     clear()
     for message in messages:
       print(message)
+    print(f"- last update: {datetime_formatted(datetime.now())}")
 
     if (percent < PERCENTAGE_THRESHOLD) and not plugged_in:
       winsound.PlaySound('./sfx/alert.wav', winsound.SND_FILENAME)
